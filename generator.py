Fix bug in logging policy videos to wandb; Fix loss calculation error

generator.py
@@ -3,6 +3,8 @@ from MP import DegeneratedMarkovProcess
 from sklearn.model_selection import train_test_split
 import utils
 import wandb
+import random
+import numpy as np
 
 class DataGenerator:
 
@@ -51,8 +53,8 @@ class DataGenerator:
                     
             if len(video_name) and i in visualize_epi:
                 wandb.log(
-                  {f"{video_name}_i": wandb.Video(frames, fps=framerate, format="mp4")})
-
+                  {f"{video_name}_i": wandb.Video(np.transpose(np.asarray(frames),(0,3,1,2)), fps=framerate, format="mp4")})
+                
             episode = torch.stack(episode,dim=0)
             data.append(episode)
 
@@ -74,7 +76,7 @@ class DataGenerator:
             train_policies = policies
             
         number_of_policies_to_visualize = 5
-        visualize_policies = set(random.sample(list(range(len(train_policies))),number_of_policies_to_visualize))
+        visualize_policies = set(random.sample(list(range(len(train_policies))),min(number_of_policies_to_visualize,len(train_policies))))
 
         state_time_series_train = []
         for i,policy in enumerate(train_policies):
@@ -82,34 +84,38 @@ class DataGenerator:
           if i not in visualize_policies:
               state_time_series_train.append(self.get_time_series(policy, n, t))
           else:
+              print("Logging this policy")
               state_time_series_train.append(self.get_time_series(policy, n, t,video_name=f"train_{i}"))
                 
           print()
         state_time_series_train = torch.cat(state_time_series_train,dim=0)
         print()
         
-        visualize_policies = set(random.sample(list(range(len(val_policies))),number_of_policies_to_visualize))
+        visualize_policies = set(random.sample(list(range(len(val_policies))),min(number_of_policies_to_visualize,len(val_policies))))
 
         state_time_series_val = []
         for i,policy in enumerate(val_policies):
           print(f"Validation policy {i+1}...")
           if i not in visualize_policies:
-              state_time_series_train.append(self.get_time_series(policy, n, t))
+              state_time_series_val.append(self.get_time_series(policy, n, t))
           else:
-              state_time_series_train.append(self.get_time_series(policy, n, t,video_name=f"val_{i}"))
+              print("Logging this policy")
+            
+              state_time_series_val.append(self.get_time_series(policy, n, t,video_name=f"val_{i}"))
           print()
         state_time_series_val = torch.cat(state_time_series_val,dim=0)
         print()
         
-        visualize_policies = set(random.sample(list(range(len(test_policies))),number_of_policies_to_visualize))
+        visualize_policies = set(random.sample(list(range(len(test_policies))),min(number_of_policies_to_visualize,len(test_policies))))
 
         state_time_series_test = []
         for i,policy in enumerate(test_policies):
           print(f"Test policy {i+1}...")
           if i not in visualize_policies:
-              state_time_series_train.append(self.get_time_series(policy, n, t))
+              state_time_series_test.append(self.get_time_series(policy, n, t))
           else:
-              state_time_series_train.append(self.get_time_series(policy, n, t,video_name=f"test_{i}"))
+              print("Logging this policy")
+              state_time_series_test.append(self.get_time_series(policy, n, t,video_name=f"test_{i}"))
           print()
         state_time_series_test = torch.cat(state_time_series_test,dim=0)
         print()

policy.py
@@ -39,9 +39,9 @@ class PolicyNetwork(nn.Module):
         self.device = utils.get_device()
 
         self.net = nn.Sequential(
-            nn.Linear(state_space_size, action_space_size),
+            nn.Linear(state_space_size, 4),
             # nn.Tanh(),
-            # nn.Linear(4, action_space_size),
+            nn.Linear(4, action_space_size),
             nn.Softmax(dim=1)
         ).to(self.device)
 

solver.py
@@ -5,58 +5,81 @@ from custom_dataset import EpisodesDataset
 from utils import shuffle
 
 def train(train_dataset, val_dataset,model,num_epochs= 100,batch_size = 32,verbose = False):
-  optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters())
 
-  train_dataset = EpisodesDataset(train_dataset)
-  train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size)
-  for epoch in range(int(num_epochs)):
-    if verbose:
-      print(f'Epoch: {epoch}',end=' ')
-    model.train()
-    for batch in train_dataloader:
-      batch_X, batch_y, evolve_lens = batch
+    train_dataset = EpisodesDataset(train_dataset)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size)
+    for epoch in range(int(num_epochs)):
+        if verbose:
+          print(f'Epoch: {epoch}',end=' ')
+        model.train()
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            batch_X, batch_y, evolve_lens = batch
 
-      train_loss = 0
-      for i,[evolve_len] in enumerate(evolve_lens):
-        X,y = shuffle(batch_X[i],batch_y[i])
+            train_loss = 0
+            preds = []
+            ys = []
+            for i,[evolve_len] in enumerate(evolve_lens):
+                X,y = shuffle(batch_X[i],batch_y[i])
 
-        pred_y = model(X,evolve_len).squeeze(-1)
+                pred_y = model(X,evolve_len).squeeze(-1)
+                
+                preds.append(pred_y)
+                ys.append(y)
+            
+            preds = torch.stack(preds,dim = 0)
+            ys = torch.stack(ys,dim=0)
+            loss = torch.nn.functional.mse_loss(preds, ys)
+            train_loss = loss.item()
+            loss.backward()
+            optimizer.step()
 
-        loss = torch.nn.functional.mse_loss(pred_y, y)
-        train_loss += loss.item()
-        loss.backward()
-    
-      optimizer.step()
-      optimizer.zero_grad()
 
-    with torch.no_grad():
-      val_loss = 0
-      model.eval()
-      for evolve_len in val_dataset:
-        X,y = val_dataset[evolve_len]
-        pred_y = model(X,evolve_len).squeeze(-1)
-        val_loss += torch.nn.functional.mse_loss(pred_y, y).item()
+        with torch.no_grad():
+            val_loss = 0
+            model.eval()
+            preds = []
+            ys = []
+            for evolve_len in val_dataset:
+                X,y = val_dataset[evolve_len]
+                pred_y = model(X,evolve_len).squeeze(-1)
+                
+                preds.append(pred_y)
+                ys.append(y)
+            
+            preds = torch.stack(preds,dim = 0)
+            ys = torch.stack(ys,dim=0)
+            val_loss = torch.nn.functional.mse_loss(preds, ys).item()
 
-    if verbose:
-      print(f'Training loss: {train_loss}',end='\t')
-      print(f'Validation loss: {val_loss}')
+        if verbose:
+            print(f'Training loss: {train_loss}',end='\t')
+            print(f'Validation loss: {val_loss}')
 
-    wandb.log({"Validation loss":val_loss},step=epoch)
-    wandb.log({"Training loss":train_loss},step=epoch)
+        wandb.log({"Validation loss":val_loss},step=epoch)
+        wandb.log({"Training loss":train_loss},step=epoch)
 
   # torch.save(model.state_dict(), 'model.pth')
   # wandb.save('model.pth')
 
 def test(test_dataset, model):
-  test_loss = 0
-  for evolve_len in test_dataset:
-    X,y = test_dataset[evolve_len]
-    pred_y = model(X,evolve_len).squeeze(-1)
-    test_loss += torch.nn.functional.mse_loss(pred_y, y).item()
+    test_loss = 0
+    preds = []
+    ys = []
+    for evolve_len in test_dataset:
+        X,y = test_dataset[evolve_len]
+        pred_y = model(X,evolve_len).squeeze(-1)
+        
+        preds.append(pred_y)
+        ys.append(y)
+        
+    preds = torch.stack(preds,dim = 0)
+    ys = torch.stack(ys,dim=0)
+    test_loss = torch.nn.functional.mse_loss(preds, ys).item()
 
-  print(f'Test loss: {test_loss}')
-  
-  wandb.log({"Test loss":test_loss})
+    print(f'Test loss: {test_loss}')
+
+    wandb.log({"Test loss":test_loss})
 
 
 